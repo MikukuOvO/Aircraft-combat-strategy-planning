000dcb2443aaeb6e77b69a6ba786242ab6e26622
MikukuOvO <mikukuovo@gmail.com>
2024-06-05 22:59:17 +0800

    Leave C++

--- generate.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# 编译C++代码
-os.system("g++ -std=c++17 main.cpp -o main")
-
-# 调用编译后的C++程序，并传入参数
-os.system("./main 1")  # 这里传入参数 1，你可以根据需要传入 0，1，2，3
-
-print("Data generation completed.")
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# 获取测试用例编号和其它参数
-testcase_number=$1
-dir=$2
-
-# 将参数传递给 Python 脚本
-python main.py "$testcase_number" "$dir"
